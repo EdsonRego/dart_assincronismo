6751514546ab93ff683cb92f1cda48b18f65c7c3
EdsonRego <edsonxrego@gmail.com>
2026-02-04 21:36:46 -0300

    first commit

---
+++ bin/dart_assincronismo.dart
@@ -0,0 +1,5 @@
+import 'package:dart_assincronismo/dart_assincronismo.dart' as dart_assincronismo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_assincronismo.calculate()}!');
+}
