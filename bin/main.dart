dd18b5a249413d30ca45db6ca3f1818da99f09a8
EdsonRego <edsonxrego@gmail.com>
2026-02-06 19:30:56 -0300

    Tratando resposta json

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() {
   print("Olá mundo assincrono!");
@@ -14,5 +15,10 @@ requestData() {
   futureResponse.then((Response response) {
     print(response);
     print(response.body);
+    List<dynamic> listAccounts = json.decode(response.body);
+    Map<String, dynamic> mapCarla = listAccounts.firstWhere(
+      (element) => element["name"] == "Carla",
+    );
+    print(mapCarla["balance"]);
   });
 }
