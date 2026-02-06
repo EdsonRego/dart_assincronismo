6f675a3fee135ef3fd2f086ee25893927f59ab63
EdsonRego <edsonxrego@gmail.com>
2026-02-06 18:49:42 -0300

    Assincronismo

--- bin/main.dart
+++ bin/main.dart
@@ -9,5 +9,6 @@ requestData() {
   String url =
       "https://gist.githubusercontent.com/EdsonRego/0c4f212c76568e778d4465242d475afa/raw/25ff3011edfed3ebcb4b7c7d6bb6d1b6ce93654e/accounts.json";
 
-  print(Uri.parse(url));
+  Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
 }
