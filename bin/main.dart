05f06802b14671cc46a02ce06aac82176b866ccc
EdsonRego <edsonxrego@gmail.com>
2026-02-06 20:06:52 -0300

    Retorno de Função Assincrona

--- bin/main.dart
+++ bin/main.dart
@@ -4,7 +4,13 @@ import 'dart:convert';
 void main() {
   print("Olá mundo assincrono!");
   //requestData();
-  requestDataAssync();
+  //requestDataAssync();
+  sendDataAsync({
+    "id": "NEW001",
+    "name": "Flutter",
+    "lastName": "Dart",
+    "balance": 5000,
+  });
 }
 
 requestData() {
@@ -26,10 +32,16 @@ requestData() {
   print("Última coisa a acontecer na função");
 }
 
-requestDataAssync() async {
+Future<List<dynamic>> requestDataAssync() async {
   String url =
       "https://gist.githubusercontent.com/EdsonRego/0c4f212c76568e778d4465242d475afa/raw/25ff3011edfed3ebcb4b7c7d6bb6d1b6ce93654e/accounts.json";
   Response response = await get(Uri.parse(url));
-  print(json.decode(response.body)[0]);
-  print("De fato! Última coisa a acontecer na função");
+  return json.decode(response.body);
+}
+
+sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAssync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+  print(content);
 }
