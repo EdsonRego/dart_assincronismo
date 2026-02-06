20b382585c83c4b8cb422deaf789e94e787b3aaa
EdsonRego <edsonxrego@gmail.com>
2026-02-06 19:49:01 -0300

    Função Assincrona

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 
 void main() {
   print("Olá mundo assincrono!");
-  requestData();
+  //requestData();
+  requestDataAssync();
 }
 
 requestData() {
@@ -23,4 +24,12 @@ requestData() {
   });
 
   print("Última coisa a acontecer na função");
+}
+
+requestDataAssync() async {
+  String url =
+      "https://gist.githubusercontent.com/EdsonRego/0c4f212c76568e778d4465242d475afa/raw/25ff3011edfed3ebcb4b7c7d6bb6d1b6ce93654e/accounts.json";
+  Response response = await get(Uri.parse(url));
+  print(json.decode(response.body)[0]);
+  print("De fato! Última coisa a acontecer na função");
 }
