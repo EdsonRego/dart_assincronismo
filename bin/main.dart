f1fe54d159b5a4578753f6549fc65fa12aecebd3
EdsonRego <edsonxrego@gmail.com>
2026-02-07 14:00:15 -0300

    Models

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,22 @@
+import 'dart:async';
+
 import 'package:dart_assincronismo/api_key.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 
+StreamController<String> streamController = StreamController<String>();
+
 void main() {
   print("Olá mundo assincrono!");
-  //requestData();
-  //requestDataAssync();
+
+  StreamSubscription streamSubscription = streamController.stream.listen((
+    String info,
+  ) {
+    print(info);
+  });
+
+  requestData();
+  requestDataAssync();
   sendDataAsync({
     "id": "NEW001",
     "name": "Flutter",
@@ -19,24 +30,16 @@ requestData() {
       "https://gist.githubusercontent.com/EdsonRego/0c4f212c76568e778d4465242d475afa/raw/25ff3011edfed3ebcb4b7c7d6bb6d1b6ce93654e/accounts.json";
 
   Future<Response> futureResponse = get(Uri.parse(url));
-  print(futureResponse);
   futureResponse.then((Response response) {
-    print(response);
-    print(response.body);
-    List<dynamic> listAccounts = json.decode(response.body);
-    Map<String, dynamic> mapCarla = listAccounts.firstWhere(
-      (element) => element["name"] == "Carla",
-    );
-    print(mapCarla["balance"]);
+    streamController.add("${DateTime.now()} | Requisição de leitura (usando then)");
   });
-
-  print("Última coisa a acontecer na função");
 }
 
 Future<List<dynamic>> requestDataAssync() async {
   String url =
       "https://gist.githubusercontent.com/EdsonRego/0c4f212c76568e778d4465242d475afa/raw/25ff3011edfed3ebcb4b7c7d6bb6d1b6ce93654e/accounts.json";
   Response response = await get(Uri.parse(url));
+  streamController.add("${DateTime.now()} | Requisição de leitura ");
   return json.decode(response.body);
 }
 
@@ -45,22 +48,23 @@ sendDataAsync(Map<String, dynamic> mapAccount) async {
   listAccounts.add(mapAccount);
   String content = json.encode(listAccounts);
 
-  String url =
-      "https://api.github.com/gists/0c4f212c76568e778d4465242d475afa";
+  String url = "https://api.github.com/gists/0c4f212c76568e778d4465242d475afa";
 
   Response response = await post(
-    Uri.parse(url), 
-    headers: {
-    "Authorization" : "Bearer $githubApiKey"}, 
+    Uri.parse(url),
+    headers: {"Authorization": "Bearer $githubApiKey"},
     body: json.encode({
-      "description" : "account.json",
-      "public" : true,
-      "files" : {
-      "accounts.json" : {
-        "content" : content,
-      }
-    }
-  }),
+      "description": "account.json",
+      "public": true,
+      "files": {
+        "accounts.json": {"content": content},
+      },
+    }),
   );
-  print(response.statusCode);
+
+  if(response.statusCode.toString()[0] == "2"){
+    streamController.add("${DateTime.now()} | Requisição bem sucedida (${mapAccount["name"]})");
+  }else{
+    streamController.add("${DateTime.now()} | Requisição bem falhou (${mapAccount["name"]})");
+  }
 }
